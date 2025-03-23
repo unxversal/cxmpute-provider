7a5a6361e2748eb8b225199f92d1e0ffa900f493
twizzworld <103385201+numinousmuses@users.noreply.github.com>
2025-03-23 17:22:24 -0500

    finished menu UI

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -34,15 +34,26 @@ const Menu = () => {
                 </div>
             </div>
             
-            
-            <div className={styles.footer}>
-                <div className={styles.footerTag}>Cxmpute Blog</div>
-                <p>Learn how to optimize your experience.</p>
-                <h3>Read Blog</h3>
-                <div className={styles.footerIcon}>
-                    <CircleArrowOutUpRight />
+            <a href="https://cxmpute.cloud/maximize" target="_blank">
+                <div className={styles.footer}>
+                    <div className={styles.footerTag}>Cxmpute Blog</div>
+                    <p>Learn how to optimize your experience.</p>
+                    <h3>Read Blog</h3>
+                    <div className={styles.footerIcon}>
+                        <svg
+                            className={styles.icon}
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                        >
+                            <path
+                            fillRule="evenodd"
+                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+                            clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     );
 };
