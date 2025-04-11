8f5292f160802dbcf9c80eee1aa705eef409f62e
twizzworld <103385201+numinousmuses@users.noreply.github.com>
2025-04-11 12:35:08 -0500

    removed app.ts to enable conditional loading of api route

--- api-sidecar/src/index.ts
+++ api-sidecar/src/index.ts
@@ -1,5 +1,33 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import app from './app';
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+import * as middlewares from './middlewares';
+import api from './api';
+import MessageResponse from './interfaces/MessageResponse';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+  });
+});
+
+app.use('/api/v1', api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
 import { tunnelmole } from 'tunnelmole';
 
 const command = process.argv[2];
